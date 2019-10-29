a218d3cd9cf93eab66f7adc6ea54a2684453eec2
James Kent <james-kent@uiowa.edu>
2019-10-29 14:43:38 -0500

    parameterize bids transform tests

--- accel_code/tests/test_bids_transform.py
+++ accel_code/tests/test_bids_transform.py
@@ -1,24 +1,38 @@
-import os
+import pytest
 
 from ..bids_transform import bids_transform
-from .. import utils
 
 
-def test_bids_transform(monkeypatch):
-    def mock_bids(path):
-        test_dir = os.path.dirname(os.path.realpath(__file__))
-        fake_server = os.path.join(
-            test_dir,
-            "data",
-            "vosslabhpc",
-            "Projects",
-            "BikeExtend",
-            "3-Experiment",
-            "2-Data",
-            "BIDS")
+@pytest.mark.parametrize(
+    "project,sub_id,ses_id,expected_path",
+    [
+        ("ALERT", "21", None,
+         ('vosslabhpc/Projects/ALERT/3-Experiment/2-Data/BIDS/'
+          'sub-21/beh/sub-21_accel.csv')),
+        ("PACR", "controlSE051", None,
+         ('vosslabhpc/Projects/PACR-AD/3-Experiment/2-Data/BIDS/'
+          'sub-controlSE051/ses-pre/beh/sub-controlSE051_ses-pre_accel.csv')),
+        ("Normative", "3795", None,
+         ('vosslabhpc/Projects/NormativeSample/3-Experiment/2-Data/BIDS/'
+          'sub-3795/beh/sub-3795_accel.csv')),
+        ("Bike_Pre", "SEA142", "pre",
+         ('vosslabhpc/Projects/Bike_ATrain/Imaging/BIDS/'
+          'sub-SEA142/ses-pre/beh/sub-SEA142_ses-pre_accel.csv')),
+        ("AMBI", "21", None,
+         ('vosslabhpc/Projects/AMBI/3-Experiment/2-Data/Imaging/BIDS/'
+          'sub-21/beh/sub-21_accel.csv')),
+        ("Bike_Post", "SEP202", "post",
+         ('vosslabhpc/Projects/Bike_ATrain/Imaging/BIDS/'
+          'sub-SEP202/ses-post/beh/sub-SEP202_ses-post_accel.csv')),
+        ("BETTER", "GE120014",  "pre",
+         ('vosslabhpc/Projects/BETTER/3-Experiment/2-data/bids/'
+          'sub-GE120014/ses-pre/beh/sub-GE120014_ses-pre_accel.csv')),
+        ("EXTEND", "2103", "3",
+         ('vosslabhpc/Projects/BikeExtend/3-Experiment/2-Data/BIDS/'
+          'sub-2103/ses-accel3/beh/sub-2103_ses-accel3_accel.csv')),
+    ])
+def test_bids_transform(project, sub_id, ses_id, expected_path):
 
-        return fake_server
+    path = bids_transform(project, sub_id, ses_id)
 
-    monkeypatch.setattr(utils, "get_test_data_path", mock_bids)
-
-    bids_transform("BikeExtend", 2103, 3)
+    assert path == expected_path
