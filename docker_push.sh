a77506748ba615d7aed5c6598bfc687c330a508e
James Kent <james-kent@uiowa.edu>
2020-04-15 10:43:43 -0500

    add missing tag step

--- docker_push.sh
+++ docker_push.sh
@@ -1,9 +1,11 @@
 echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
 # if tag is defined
 if [[ ! -z "$TRAVIS_TAG" ]]; then
+    docker tag hbclab/accel-bids:latest hbclab/accel-bids:$TRAVIS_TAG
     docker push hbclab/accel-bids:$TRAVIS_TAG
 # if the build is on the master branch
 elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    docker tag hbclab/accel-bids:latest hbclab/accel-bids:unstable
     docker push hbclab/accel-bids:unstable
 else
     echo "This build is not on the master branch or a tagged release"
