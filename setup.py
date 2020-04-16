1f2db4a75e8643a21903915ca0c0a0d024c5c5c5
James Kent <james-kent@uiowa.edu>
2020-04-16 15:30:03 -0500

    add entrypoint to the script

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name="accel-code", packages=find_packages())
+setup(
+    name="accel-code", packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "accel_transform = accel_code.run:main",
+        ],
+    },
+)
